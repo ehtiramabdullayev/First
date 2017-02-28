bbba497bf53973841098449deedf3d41d463e7fd
Ehtiram Abdullayev <ehtiramabdullayev@gmail.com>
2017-03-01 00:18:08 +0400

    --first commit

---
+++ FirstPro/src/com/main/first/Main.java
@@ -0,0 +1,11 @@
+package com.main.first;
+
+/**
+ * Created by Master on 3/1/2017.
+ */
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println("hello lool");
+    }
+}
